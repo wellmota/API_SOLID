a422433c056c82d291dce1e52fc11abc9aaec990
wellmota <impeto.wellington@gmail.com>
2025-07-20 21:26:29 -0300

    fix: update PrismaClient import path to match custom output location

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import { fastify } from 'fastify'
+import { PrismaClient } from '../generated/prisma'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Wellington Mota',
+    email: 'well@email.com',
+  },
+})
